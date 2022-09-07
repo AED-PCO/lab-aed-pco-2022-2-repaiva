bb579888db3cf0365c1ef5b84438dab6f9932f3f
Roberto Eller <repaiva@sga.pucminas.br>
2022-09-07 13:41:37 -0300

    Questão 6 Lab2

--- codigo/Roteiro 02/EX06/Program.cs
+++ codigo/Roteiro 02/EX06/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Digite a palavra para remover todas as vogais: ");
+string palavra = Console.ReadLine();
+string resultado = VogDelet(palavra);
+Console.WriteLine("A palavra {0} sem vogais fica {1}", palavra, resultado);
+
+static string VogDelet(string texto)
+{
+    int pos= texto.Length-1;
+    if (texto == "") 
+        return "";
+    if (texto[pos] == 'a' || texto[pos] == 'e' || texto[pos] == 'i' || texto[pos] == 'o' || texto[pos] == 'u') 
+        return VogDelet(texto.Substring(0, pos));
+    return VogDelet(texto.Substring(0, pos))+texto[pos];
+}
