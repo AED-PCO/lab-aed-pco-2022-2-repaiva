9a9f0fa27c73af91667b097ce14b189b0803dc3b
Roberto Eller <repaiva@sga.pucminas.br>
2022-11-08 19:57:00 -0300

    Quest 1 lab 9

---
+++ codigo/Roteiro 09/EX01/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{   
+    static void inserirVal (int[] vet, int val, ref int aux)
+        {           
+            vet[aux] = val;
+            aux++;
+        }
+            
+      static int removerVal (int[] vet, ref int aux)
+        {  
+            aux--;
+            return vet[aux];    
+        }
+
+    static void imprimeVet (int []vet, int aux)
+    {
+        int j = aux;
+        int [] VetAux = new int [vet.Length];
+        for(int i = 0; j>0; i++){
+            VetAux[i] = vet[j-1];
+            j--;
+        }
+         for(int i = 0; i<aux; i++)
+            Console.Write(VetAux[i]+"\t");
+        Console.WriteLine();
+    }    
+    
+    static void PilhaParaPilha (int []vet, ref int aux)
+    {
+        int [] PilhaAux = new int[vet.Length];
+        int [] resultado = new int[vet.Length];
+        int referencia = 0;
+        int referencia2 = 0;
+        for(int i = aux; i > 0; i--)
+        {
+            int removido = removerVal(vet, ref aux);
+            inserirVal(PilhaAux, removido, ref referencia);
+        }
+        for(int i = referencia; i > 0; i--)
+        {
+            int removido = removerVal(PilhaAux, ref referencia);
+            inserirVal(vet, removido, ref aux);
+            inserirVal(resultado, removido, ref referencia2);
+        }
+        Console.WriteLine("Pilha de Entrada:");
+        imprimeVet(vet, aux);
+        Console.WriteLine("Pilha de Saída:");
+        imprimeVet(resultado, referencia2);
+        
+    }
+   static void Main(string[] args)
+    {
+        Console.WriteLine("LAB 09 - EX01");
+        Console.WriteLine("Informe o tamanho do vetor desejado (maior que zero)");
+        int tam = int.Parse(Console.ReadLine());
+        int[] vetor = new int[tam];
+        int []resultado;
+        int referencia = 0;
+        Console.WriteLine("Informe o valor que deseja inserir na pilha (-1 termina)");
+        int val = int.Parse(Console.ReadLine());
+        while(val != -1)
+        {
+        inserirVal(vetor, val, ref referencia);
+        Console.WriteLine("Informe o valor que deseja inserir na pilha (-1 termina)");
+        val = int.Parse(Console.ReadLine());
+        }
+        PilhaParaPilha(vetor, ref referencia);
+    }
+}
---
+++ relatorio/Roteiro 09/README.md
@@ -0,0 +1,11 @@
+## Questão 01 - Copiar uma pilha para outra pilha mantendo na ordem original.
+
+Nessa questão, no próprio progrma principal "main" fazemos um loop chamando a função de iserir valor na pilha com flag de -1, assim temos uma pilha inicial. Após isso, chamamos a função "PilhaParaPilha" que contém a solução do problema de fato. Nesta função, temos uma pilha auxiliar onde será inserido os valores removidos das pilha original, como na pilha só conseguimos remover valores da última posição, a pilha auxiliar tera os valores da pilha original de traz para frente. Na próxima etapa inserimos os valores dessa pilha auxiliar na pilha original e na pilha de saída, denovo, seguindo a filosofia da pilha, as pilhas original e de saída receberam os valores da pilha auxiliar de trás para frente, agora sim na ordem da pilha inicializada pelo usuário. Observação: os valores impressos no console mostram os valores da pilha de trás para frente pois é assim que a pilha enxerga.  
+
+![Print EX 01](img/EX01.png)
+
+## Questão 02 - Copiar uma fila para outra fila invertendo a ordem original.
+
+
+
+![Print EX 02](img/EX02.png)
