4cf9ea580e78923fcdc4adf9b53a57d1ce6caa3e
Roberto Eller <repaiva@sga.pucminas.br>
2022-12-15 17:15:25 -0300

    Melhorias LAB 12

--- codigo/Roteiro 12/EX01/Program.cs
+++ codigo/Roteiro 12/EX01/Program.cs
@@ -4,70 +4,84 @@ internal class Program
     private static void Main(string[] args)
     {
         no raiz = new no();
-        int reg = -1;
-        Console.WriteLine("Informe o valor que deseja inserir");
-        reg = int.Parse(Console.ReadLine());
-        while(reg != -1)
+        int flag = -1;
+        while(flag != 0)
         {
-            arvore.porValor.inserir(ref raiz, reg);
-            Console.WriteLine("Informe o valor que deseja inserir");
-            reg = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("ESCOLHA UMA OPÇÃO: \n 1 - Inserir \n 2 - Em Ordem \n 3 - Pré Ordem \n 4 - Pos Ordem \n 0 - terminar");
+            flag = int.Parse(Console.ReadLine());
+            switch(flag)
+            {
+                case 1: insere.Chamainserir(raiz); Console.WriteLine(); break;
+                case 2: impressao.emOrdem(raiz); Console.WriteLine(); break;
+                case 3: impressao.PreOrdem(raiz); Console.WriteLine(); break;
+                case 4: impressao.PosOrdem(raiz); Console.WriteLine(); break;
+            }
         }
-        string[] resultado = arvore.metodosComuns.emOrdem(ref raiz).Split("$");
-        for(int i = resultado.Length-2; i > -1; i--)
-            Console.Write(resultado[i]+"\t");
     }
 }
 
 namespace arvore{
-    class no{
-        public int registro;
-        public no filhoEsquerda;
-        public no filhoDireita;
-
-        public no(){
-            this.filhoEsquerda = null;
-            this.filhoDireita = null;
-        }
+class no{
+    public int registro;
+    public no filhoEsquerda;
+    public no filhoDireita;
+    public no()
+    {
+        this.filhoEsquerda = null;
+        this.filhoDireita = null;
     }
+}
 
-    class metodosComuns{
-        public static string emOrdem(ref no raiz)
+class impressao{
+        public static void emOrdem(no raiz)
         {
-            string resultado;
+            if (raiz!=null) {
+                emOrdem(raiz.filhoEsquerda);
+                Console.Write(raiz.registro+"\t");
+                emOrdem(raiz.filhoDireita);
+            }
+        }
+        public static void PreOrdem(no raiz) {
+        if (raiz!=null) {
+            Console.Write(raiz.registro+"\t");
+            PreOrdem(raiz.filhoEsquerda);
+            PreOrdem(raiz.filhoDireita);
+        }
+    }
+    public static void PosOrdem(no raiz) {
+        if (raiz!=null) {            
+            PosOrdem(raiz.filhoEsquerda);
+            PosOrdem(raiz.filhoDireita);
+            Console.Write(raiz.registro+"\t");
+        }
+    }
+}
+class insere{
+    public static void Chamainserir(no raiz)
+    {
+        Console.WriteLine("Informe o valor que deseja inserir");
+        int reg = int.Parse(Console.ReadLine());
+        inserir(raiz, reg);
+    }
+    public static void inserir(no raiz, int reg){
+        no novo = new no();
+        novo.registro = reg;
+        if(raiz.registro < novo.registro){
             if(raiz.filhoEsquerda == null)
-                resultado = raiz.registro + "$";
-        
+                raiz.filhoEsquerda = novo;
+                
             else
-                resultado = emOrdem(ref raiz.filhoEsquerda) + raiz.registro + "$";
+                inserir(raiz.filhoEsquerda, reg);  
+        }
         
-            if(raiz.filhoDireita != null)
-                resultado += emOrdem(ref raiz.filhoDireita) + "$";
+        else{
+            if(raiz.filhoDireita == null)
+                raiz.filhoDireita = novo;
             
-            return resultado;
+            else
+                inserir(raiz.filhoDireita, reg);
         }
     }
-
-    class porValor{
-        public static void inserir(ref no raiz, int reg){
-            no novo = new no();
-            novo.registro = reg;
-
-            if(raiz.registro < novo.registro){
-                if(raiz.filhoEsquerda == null)
-                    raiz.filhoEsquerda = novo;
-                
-                else
-                    inserir(ref raiz.filhoEsquerda, reg);
-                
-            }
-            else{
-                if(raiz.filhoDireita == null)
-                    raiz.filhoDireita = novo;
-                
-                else
-                    inserir(ref raiz.filhoDireita, reg);
-            }
-        }
-    }
+}
 }
--- relatorio/Roteiro 12/README.md
+++ relatorio/Roteiro 12/README.md
@@ -1,5 +1,5 @@
 ## Questão 01 - ÁRVORE ABB.
 
-Nessa questão criamos a classe "no" que possui dois filhos, um a esquerda e outra a direita, ambos apontando para nulo de início. há também a função inserir, que insere os valores desejado pelo usuário. Na função inserir fazemos teste de maior e menor para sabermos se vamos posicionar o valor a inserir a direita ou a esquerda do no raiz, e o no raiz vai atualizando a cada chamada recursiva. Por fim temos a função emOrdem que faz a leitura dos dados em ordem e armazena em um vetor após isso só imprimimos o vetor no main.
+Nessa questão criamos a classe "no" que possui dois filhos, um a esquerda e outra a direita, ambos apontando para nulo de início. há também a função inserir, que insere os valores desejado pelo usuário. Na função inserir fazemos teste de maior e menor para sabermos se vamos posicionar o valor a ser inserido à direita ou à esquerda do no raiz, e o no raiz vai atualizando a cada chamada recursiva. Por fim temos as funções de impressão Em Ordem, Pos Ordem e Pré Ordem.
 
 ![Print EX 01](img/EX01.png)
